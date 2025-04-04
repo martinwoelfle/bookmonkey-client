d0306d7c082e96f26fbb09ec716efd4eac9d34d3
Martin Wölfle <mwoelfle@arz-emmendingen.de>
2025-04-04 11:21:21 +0200

    Try better filter-logic

--- bookmonkey-client/src/app/book/book-filter/book-filter.pipe.ts
+++ bookmonkey-client/src/app/book/book-filter/book-filter.pipe.ts
@@ -15,6 +15,11 @@ export class BookFilterPipe implements PipeTransform {
   }
 }
 
-// function filterBook(books: Book[], searchTerm: string): boolean {
-//   Object.entries(books).forEach(([key, value]) => {value})
-// }
+ // function filterBook(books: Book, searchTerm: string): boolean {
+ //   Object.entries(books).some(([key, value]) => {
+ //     if (typeof searchTerm === 'string') {
+ //       return value.ToLowerCase().includes(searchTerm.toLowerCase());
+ //     }
+ //     return false;
+ //   })
+ // }
