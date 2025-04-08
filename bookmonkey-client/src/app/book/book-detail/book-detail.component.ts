d40f8bdb26fd3c548d754a0594d55c2023a03f2b
Martin Wölfle <mwoelfle@arz-emmendingen.de>
2025-04-08 12:34:49 +0200

    Angular-Routing 2 Improve (Add book-detail route, Signal)

--- bookmonkey-client/src/app/book/book-detail/book-detail.component.ts
+++ bookmonkey-client/src/app/book/book-detail/book-detail.component.ts
@@ -1,7 +1,9 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {BookApiService} from '../services/book-api.service';
 import {ActivatedRoute} from '@angular/router';
-import {Book} from '../models/book';
+// import {Book} from '../models/book';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {catchError, of, switchMap, tap, debounceTime} from 'rxjs';
 
 @Component({
   selector: 'app-book-detail',
@@ -10,17 +12,30 @@ import {Book} from '../models/book';
   styleUrl: './book-detail.component.scss',
   providers: [BookApiService]
 })
-export class BookDetailComponent implements OnInit {
+export class BookDetailComponent {
   private readonly route = inject(ActivatedRoute);
   private readonly bookApi = inject(BookApiService);
+  protected loading = false;
 
-  protected book: Book | undefined;
+  // protected book: Book | undefined;
 
-  ngOnInit() {
-    this.route.params.subscribe(((params) => {
-      const isbn = params['isbn'];
-      this.bookApi.getByIsbn(isbn).subscribe(bookFromApi =>
-        this.book = bookFromApi);
-    }))
-  }
+  protected bookSignal = toSignal(
+    this.route.params.pipe(
+      tap(() => (this.loading = true)),
+      debounceTime(2_000),
+      switchMap((params) => {
+        const isbn = params['isbn'];
+
+        return this.bookApi.getByIsbn(isbn).pipe(catchError(() => of(undefined)))
+      })
+    ))
+
+  // ngOnInit() {
+  //   // this.route.params.subscribe(((params) => {
+  //   //   const isbn = params['isbn'];
+  //   //   this.bookApi.getByIsbn(isbn).subscribe(bookFromApi =>
+  //   //     this.book = bookFromApi);
+  //   // }))
+  // }
 }
+
