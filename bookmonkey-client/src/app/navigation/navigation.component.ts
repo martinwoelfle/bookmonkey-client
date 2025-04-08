5561bf4c35bf2c7a1cf16455af8be3dcb995bdc5
Martin Wölfle <mwoelfle@arz-emmendingen.de>
2025-04-08 10:01:03 +0200

    Angular-Routing 1 Bonus (Add basic routing with navigation-style)

--- bookmonkey-client/src/app/navigation/navigation.component.ts
+++ bookmonkey-client/src/app/navigation/navigation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
